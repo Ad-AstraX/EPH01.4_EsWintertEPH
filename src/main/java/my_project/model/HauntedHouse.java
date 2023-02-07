d5d198d32ee0c34b72c98c211df8ee2dae3e99c7
rukimatt <rukimatt@kkg-do.eu>
2023-02-07 10:11:03 +0100

    gvczzdrgh

--- src/main/java/my_project/model/HauntedHouse.java
+++ src/main/java/my_project/model/HauntedHouse.java
@@ -13,6 +13,14 @@ public class HauntedHouse extends GraphicalObject {
     }
 
     public void draw(DrawTool drawTool) {
+        drawTool.setCurrentColor(new Color(49, 49, 49));
+        drawTool.drawFilledRectangle(150, 130, 500, 300);
+        drawTool.setCurrentColor(new Color(10, 10, 10));
+        drawTool.drawFilledRectangle(235, 235, 100, 100);
+        drawTool.drawFilledRectangle(435, 235, 100, 100);
+        drawTool.setCurrentColor(new Color(183, 5, 2));
+        drawTool.drawFilledEllipse(260, 270, 5, 2);
+        drawTool.drawFilledEllipse(275, 270, 5, 2);
 
     }
 }
